106c2f0f547337d78b930974e624865eb53592e2
Kirill So <kirill.sofronov@gmail.com>
2023-08-31 08:34:35 +0800

    Refactor code for highlighting checked option and getting matching cat array

--- module-5/js-cat-meme/script.js
+++ module-5/js-cat-meme/script.js
@@ -1,14 +1,30 @@
 import { catsData } from './data.js'
 
 const radios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+
+getImageBtn.addEventListener('click', getMatchingCatsArray)
 
 radios.addEventListener('change', highlightCheckedOption)
 
 function highlightCheckedOption(e) {
+	const radios = document.getElementsByClassName('radio')
+	for (let radio of radios) {
+		radio.classList.remove('highlight')
+	}
 	document.getElementById(e.target.id).parentElement.classList.add('highlight')
-	const radios = document.getElementsByClassName('highlight')
-	for (let radio in radios) {
-		radio.parentElement.classList.remove('highlight')
+}
+
+function getMatchingCatsArray() {
+	if (document.querySelector('input[type="radio"]:checked')) {
+		const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+		const isGif = gifsOnlyOption.checked
+
+		const filteredCatsArray = catsData.filter((cat) => {
+			return cat.emotionTags.includes(selectedEmotion)
+		})
+		console.log(filteredCatsArray)
 	}
 }
 
